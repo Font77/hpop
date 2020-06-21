49783e48d7a85ab1166e246c9ca8abcbb99ce379
Font77 <vimalkg@gmail.com>
2020-06-21 22:41:42 +0530

    vk.com/etphor

--- app/src/main/java/org/kiibord/hpop/Main.java
+++ app/src/main/java/org/kiibord/hpop/Main.java
@@ -1,21 +1,4 @@
-/*
- * Copyright (C) 2011 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package org.kiibord.hpop;
-
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
@@ -32,7 +15,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.TextView.BufferType;
-
 public class Main extends Activity {
 //public class Main extends AppCompatActivity {
 
@@ -45,7 +27,7 @@ public class Main extends Activity {
         fontsovArride.setDefaultFont(this,"MONOSPACE",R.font.roboto_heks_jk);
         setContentView(R.layout.main);
         String html = getString(R.string.main_body);
-        html += "<p><i>Version: " + getString(R.string.auto_version) + "</i></p>";
+        html += "<p><i>vrjqn: " + getString(R.string.auto_version) + "</i></p>";
         Spanned content = Html.fromHtml(html);
         TextView description = (TextView) findViewById(R.id.main_description);
         description.setMovementMethod(LinkMovementMethod.getInstance());
@@ -66,28 +48,20 @@ public class Main extends Activity {
                 mgr.showInputMethodPicker();
             }
         });
-        
         final Activity that = this;
-
         final Button setup4 = (Button) findViewById(R.id.main_setup_btn_input_lang);
         setup4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivityForResult(new Intent(that, InputLanguageSelection.class), 0);
+            startActivityForResult(new Intent(that, InputLanguageSelection.class), 0);
             }
         });
 
         final Button setup3 = (Button) findViewById(R.id.main_setup_btn_get_dicts);
         setup3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URI));
-                try {
-                	startActivity(it);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(getApplicationContext(),
-                            getResources().getString(
-                            		R.string.no_market_warning), Toast.LENGTH_LONG)
-                            .show();
-                }
+            Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URI));
+            try { startActivity(it); } catch (ActivityNotFoundException e)
+            { Toast.makeText(getApplicationContext(), getResources().getString(R.string.no_market_warning), Toast.LENGTH_LONG).show(); }
             }
         });
         // PluginManager.getPluginDictionaries(getApplicationContext()); // why?
