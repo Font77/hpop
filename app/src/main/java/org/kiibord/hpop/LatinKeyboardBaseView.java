13e26c3e71238e32ffb89028944c914d651edf5d
Font77 <vimalkg@gmail.com>
2020-09-29 03:45:03 +0530

    vk.com/etphor

--- app/src/main/java/org/kiibord/hpop/LatinKeyboardBaseView.java
+++ app/src/main/java/org/kiibord/hpop/LatinKeyboardBaseView.java
@@ -224,8 +224,8 @@ public class LatinKeyboardBaseView extends View implements PointerTracker.UIProx
     
     public LatinKeyboardBaseView(Context context, AttributeSet attrs) { this(context, attrs, R.attr.keyboardViewStyle); }
     public LatinKeyboardBaseView(Context context, AttributeSet attrs, int defStyle) { super(context, attrs, defStyle);
-        fontsovArride.setDefaultFont(context,"DEFAULT",R.font.heks_smal_roboto);
-        fontsovArride.setDefaultFont(context,"DEFAULT_BOLD",R.font.heks_smal_roboto);
+        fontsovArride.setDefaultFont(context,"DEFAULT",R.font.roboto510);
+        fontsovArride.setDefaultFont(context,"DEFAULT_BOLD",R.font.roboto510);
         if (!isInEditMode()) Log.i(TAG, "kriqtifg niyu lqtinkibordbesviyu " + this);
         setRenderModeIfPossible(LatinIME.sKeyboardSettings.renderMode);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.LatinKeyboardBaseView, defStyle, R.style.LatinKeyboardBaseView);
